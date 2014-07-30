d9bd35770d944ef59a5f18268a4339ae7b59ed2c
MatthewPlewa <Matthewplewa@gmail.com>
2014-07-29 19:11:22 -0500

    New processor
    Added new processing thread

--- mobile/src/main/java/com/matthewplewa/cosmicraydetector/decov2/DataProcessor.java
+++ mobile/src/main/java/com/matthewplewa/cosmicraydetector/decov2/DataProcessor.java
@@ -4,10 +4,11 @@ package com.matthewplewa.cosmicraydetector.decov2;
  * Created by Matthew on 7/28/2014.
  */
 
-import java.io.IOException;
-
-import android.media.Image;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
+
+import java.io.File;
 
 public class DataProcessor extends Thread {
 
@@ -24,17 +25,58 @@ public class DataProcessor extends Thread {
         }
 
 
-        private static boolean go=false;
+        private boolean go=false;
+        private String path;
 
 
-        public void setImage(Image img){
-
+        public void setImage(String Path){
+            path=Path;
+            go=true;
         }
 
 
 
         public  void run(){
+            boolean keep = true;
 
+            Bitmap bit;
+            while(keep) {
+                while (!go) {
+                    try {
+                        wait(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                while(go){
+
+                    bit = BitmapFactory.decodeFile(path);
+                    int hight=bit.getHeight();
+                    int width = bit.getWidth();
+
+                    for(int x =0; x < width; x++){
+                        for(int y =0; y<hight;y++){
+                            String pic;
+                            pic =""+bit.getPixel(x,y);
+
+                            int r = pic.charAt(2)*10+pic.charAt(3);
+
+                            int g =pic.charAt(4)*10+pic.charAt(5);
+                            int b=pic.charAt(6)*10+pic.charAt(7);
+                            System.out.println(r);// must remove later
+                            if(r<50&&g<50&&b<50){
+
+                                File file = new File(path);
+                                boolean deleted = file.delete();
+
+                            }
+
+
+                        }
+                    }
+
+                }
+            }
         }
 
 
