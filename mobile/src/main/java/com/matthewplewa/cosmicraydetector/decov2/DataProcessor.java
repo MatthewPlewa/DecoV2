e77436f2087b9d8f552fea86cb7c4c9ac05fb6fb
MatthewPlewa <Matthewplewa@gmail.com>
2014-07-30 13:18:44 -0500

    testing

--- mobile/src/main/java/com/matthewplewa/cosmicraydetector/decov2/DataProcessor.java
+++ mobile/src/main/java/com/matthewplewa/cosmicraydetector/decov2/DataProcessor.java
@@ -32,51 +32,48 @@ public class DataProcessor extends Thread {
         public void setImage(String Path){
             path=Path;
             go=true;
+            told=false;
+        }
+    boolean keep = true;
+
+
+
+    Bitmap bit;
+
+        public void process(){
+            bit = BitmapFactory.decodeFile(path);
+            int hight=bit.getHeight();
+            int width = bit.getWidth();
+
+            for(int x =0; x < width; x++){
+                for(int y =0; y<hight;y++){
+                    int pic;
+                    pic =bit.getPixel(x,y);
+                    if(!told) {
+                        System.out.println(pic);
+                        told=true;
+                    }
+                    if(pic<-1000000){//delete if not above cut
+
+                        File file = new File(path);
+                        if( file.delete())
+                            Log.i("tag","deleted");
+                    }
+
+
+                }
+            }
+
+
+
         }
 
 
 
+    boolean told=false;
         public  void run(){
-            boolean keep = true;
-
-            Bitmap bit;
-            while(keep) {
-                while (!go) {
-                    try {
-                        wait(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                while(go){
-
-                    bit = BitmapFactory.decodeFile(path);
-                    int hight=bit.getHeight();
-                    int width = bit.getWidth();
-
-                    for(int x =0; x < width; x++){
-                        for(int y =0; y<hight;y++){
-                            String pic;
-                            pic =""+bit.getPixel(x,y);
-
-                            int r = pic.charAt(2)*10+pic.charAt(3);
-
-                            int g =pic.charAt(4)*10+pic.charAt(5);
-                            int b=pic.charAt(6)*10+pic.charAt(7);
-                            System.out.println(r);// must remove later
-                            if(r<50&&g<50&&b<50){
-
-                                File file = new File(path);
-                                boolean deleted = file.delete();
-
-                            }
 
 
-                        }
-                    }
-
-                }
-            }
         }
 
 
