12643a89635a736e1ece344aaf9ebba565ec336f
MatthewPlewa <Matthewplewa@gmail.com>
2014-07-31 13:46:30 -0500

    Fixed data processor
    made the Queue intelegent so that we dont get a back up. Scales based
    off peramaters set by the intelegent Queue.

--- mobile/src/main/java/com/matthewplewa/cosmicraydetector/decov2/DataProcessor.java
+++ mobile/src/main/java/com/matthewplewa/cosmicraydetector/decov2/DataProcessor.java
@@ -41,6 +41,7 @@ public class DataProcessor extends Thread {
             go = true;
             running = true;
         }
+        Log.i(tag,paths.size()+" in Queue");
 
 
     }
@@ -48,6 +49,7 @@ public class DataProcessor extends Thread {
     private void save(byte[] bytes) throws IOException {
 
         try {
+            Log.i(tag,"saving");
             Calendar c = Calendar.getInstance();
             int year = c.get(Calendar.YEAR);
             int month= c.get(Calendar.MONTH)+1;
@@ -80,11 +82,14 @@ public class DataProcessor extends Thread {
             String pic =""+ year+Month+Day+"_"+Hour+Min+Seconds;
 
             File file = new File(Environment.getExternalStorageDirectory(),"DECO/"+pic+ ".jpg");
+            Log.i(tag,"file made");
             output = new FileOutputStream(file);
             output.write(bytes);
+            Log.i(tag,"witten");
             output.flush();
             output.close();
             output=null;
+            Camera2BasicFragment.numEvents++;
 
 
 
@@ -97,7 +102,10 @@ public class DataProcessor extends Thread {
         }
     }
     Bitmap bits;
-String tag = "processor";
+    String tag = "processor";
+    int scaleX=10;
+    int scaleY=10;
+    int tempBuffer;
 
     public void process() {
         ready=false;
@@ -106,22 +114,24 @@ String tag = "processor";
         Log.i(tag,"image in");
         paths.remove(0);
 
-        Log.i("tag","Decoding");
+        Log.i(tag,"Decoding");
         BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
         bitmapOptions.inPreferredConfig= Bitmap.Config.ARGB_8888;
         bits = BitmapFactory.decodeByteArray(bytes,0,bytes.length,bitmapOptions);
         Bitmap bit = bits.copy(Bitmap.Config.ARGB_8888,true);
-        bit.reconfigure(408,308, Bitmap.Config.ARGB_8888);
-        Log.i("tag","decoded");
+        bit.reconfigure(bit.getWidth()/scaleX,bit.getHeight()/scaleY, Bitmap.Config.ARGB_8888);
+        Log.i(tag,"decoded");
         told = false;
         int hight = bit.getHeight();
 
         int width = bit.getWidth();
-
+        int rTemp=0;
+        int gTemp=0;
+        int bTemp=0;
         boolean good = false;
 
-        for (int x = 0; x < width; x+=2) {
-            for (int y = 0; y < hight; y+=2) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < hight; y++) {
                 int pic;
 
 
@@ -131,33 +141,55 @@ String tag = "processor";
                 int b = Integer.parseInt(col.substring(5, 7), 16);
                 int g = Integer.parseInt(col.substring(7, 9), 16);
                 if (r > 50||b>50||g>50 )
+                    good=true;
+                if(r>rTemp)
+                    rTemp=r;
+                if(g>gTemp)
+                    gTemp=g;
+                if(b>bTemp)
+                    bTemp=b;
 
-                    try {
-                        save(bytes);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                if (!told) {
-                    System.out.println(r + " "+ g + " "+b);
-                    System.out.println(col);
-                    told = true;
-                }
+
+
 
 
             }
 
 
         }
-        Log.i("tag","image done");
-        Log.i("tag",good+"");
-        if (!good) {//delete if not above cut
 
-            //File file = new File(test);
-            //if (file.delete())
-                Log.i("tag", "deleted");
+        Log.i(tag,"Max r g b values"+rTemp+","+gTemp+","+bTemp);
+        Log.i(tag,"image done");
+        Log.i(tag,good+"");
+        if (good) {//delete if not above cut
+
+            try {
+                save(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-         go=true;
-
+        /*
+        this sets up intelegent scaleing so that we can always have the highest resolution possible without having to worry about
+        having to large a quoue build up.
+         */
+        if(paths.size()>0) {
+            ready = true;
+            if(paths.size()>3){
+                if(tempBuffer<paths.size()) {
+                    scaleX++;
+                    scaleY++;
+                }
+            }
+        }
+        else if(paths.size()==0) {
+            ready = false;
+            scaleX--;
+            scaleY--;
+        }
+        Camera2BasicFragment.inQuaue=paths.size();
+        Log.i(tag,scaleX+","+scaleY+","+bit.getWidth()+","+bit.getHeight());
+        go=true;
         bit.recycle();
 
     }
