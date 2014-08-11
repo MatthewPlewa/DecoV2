20762d970deb081dcb776c01de28e37a401b2222
MatthewPlewa <Matthewplewa@gmail.com>
2014-08-11 16:12:28 -0500

    initial
    Getting ready to create a calibration system.

---
+++ mobile/src/main/java/com/matthewplewa/cosmicraydetector/decov2/Calibrate.java
@@ -0,0 +1,11 @@
+package com.matthewplewa.cosmicraydetector.decov2;
+
+/**
+ * Created by Matthew on 8/11/2014.
+ * The goal of this class is to create a calibrtion system that will be able to determine the proper
+ * x and y scaling that needs to be done inorder to have a quaue of one or less. This will also set up
+ * the r g and b values that will trigger the system to run a full image filter rather than a scaled image filter.
+ * This should be run for 25 or more photos to get a decent aproximation of the values needed.
+ */
+public class Calibrate {
+}
